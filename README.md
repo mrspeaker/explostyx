17885c8c28c2c8b6d21fe9568086a7f88d93bce2
Mr Speaker <mrspeaker@gmail.com>
2017-07-22 08:19:43 -0400

    remove es5 hacks

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # Explostyx
 
 by Mr Speaker. [See them styx fly](http://www.mrspeaker.net/dev/explostyx).
-
-
-## Compile to olden days JavaScript:
-
-    babel explostyx.js -o build.js --source-maps
--- explostyx.js
+++ explostyx.js
@@ -1,7 +1,4 @@
-(function (THREE) {
-
-  'use strict';
-
+(function(THREE) {
   let width;
   let height;
 
@@ -11,7 +8,7 @@
   const r = new THREE.WebGLRenderer({ antialias: true });
   const dom = r.domElement;
   document.body.appendChild(dom);
-  dom.style.position = 'absolute';
+  dom.style.position = "absolute";
   dom.style.top = dom.style.left = 0;
 
   // Handle setting window size, and resize
@@ -22,39 +19,50 @@
     camera.updateProjectionMatrix();
     r.setSize(width, height);
   };
-  window.addEventListener('resize', resize, false);
+  window.addEventListener("resize", resize, false);
   resize();
 
   // Track the mouse position
   const mouse = { x: width * 0.5, y: height * 0.5 };
-  window.addEventListener('mousemove', e => { mouse.x = e.clientX; mouse.y = e.clientY; }, false);
-  window.addEventListener('mouseout', () => { mouse.x = width * 0.5; mouse.y = height * 0.5; }, false);
-  window.addEventListener('touchmove', e => {
-    e.preventDefault();
-    const {clientX, clientY} = e.targetTouches[0];
-    mouse.x = clientX;
-    mouse.y = clientY;
-  }, false);
+  window.addEventListener(
+    "mousemove",
+    e => {
+      mouse.x = e.clientX;
+      mouse.y = e.clientY;
+    },
+    false
+  );
+  window.addEventListener(
+    "mouseout",
+    () => {
+      mouse.x = width * 0.5;
+      mouse.y = height * 0.5;
+    },
+    false
+  );
+  window.addEventListener(
+    "touchmove",
+    e => {
+      e.preventDefault();
+      const { clientX, clientY } = e.targetTouches[0];
+      mouse.x = clientX;
+      mouse.y = clientY;
+    },
+    false
+  );
 
   // Make some styx to explo!
   const geometry = new THREE.BoxGeometry(0.1, 1, 0.1);
   const material = new THREE.MeshLambertMaterial({ color: 0xf0f0a6 });
 
-  const cubes = [true]
-
-    // Array.fill not supported in chrome yet :(
-    .reduce((ac, e) => {
-      while (ac.length < 650) { ac.push(e); }
-      return ac;
-    },[])
-
+  const cubes = Array(650)
+    .fill(0)
     // Pick random start pos
     .map((c, i) => {
       const x = Math.cos(i) * 3.5;
       const y = Math.sin(i) * 2;
       return [x, y, -11];
     })
-
     // Create the mesh and set individual parameters
     .map((pos, i) => {
       const cube = new THREE.Mesh(geometry, material);
@@ -66,14 +74,13 @@
       cube.userData = {
         vel: new THREE.Vector3(),
         dir: new THREE.Vector3(),
-        rotSpeed: (1 + (Math.random())) * 0.05,
-        maxVel: 0.1 + (Math.random() * 0.4),
-        fric: 0.992 + (Math.random() * 0.005)
+        rotSpeed: (1 + Math.random()) * 0.05,
+        maxVel: 0.1 + Math.random() * 0.4,
+        fric: 0.992 + Math.random() * 0.005
       };
 
       return cube;
     })
-
     // Add them all to the scene
     .map(c => {
       scene.add(c);
@@ -96,20 +103,20 @@
   const mouseVec = new THREE.Vector3();
   const pos = new THREE.Vector3();
 
-
-  let last, start, dt;
+  let last, dt;
   let cpu_run = 0;
 
-  (function tick (t) {
-
-    if (!last) { last = start = t; }
+  (function tick(t) {
+    if (!last) {
+      last = t;
+    }
     dt = t - last;
     last = t;
 
-    // Dodgy CPU fixer: remove geom until it runs ok XD
+    // Dodgy CPU fixer: remove geom until it runs ok on mobile browsers XD
     if (dt > 35) {
       if (cpu_run++ > 5 && cubes.length > 30) {
-        const remove = cubes.length * 0.5 | 0;
+        const remove = (cubes.length * 0.5) | 0;
         for (let i = 0; i < remove; i++) {
           const last = cubes[cubes.length - 1];
           scene.remove(last);
@@ -122,7 +129,7 @@
     }
 
     // Camera-to-mouse distance
-    mouseVec.set((mouse.x / width) * 2 - 1, -(mouse.y / height) * 2 + 1, 0.5);
+    mouseVec.set(mouse.x / width * 2 - 1, -(mouse.y / height) * 2 + 1, 0.5);
     mouseVec.unproject(camera);
     const mouseDir = mouseVec.sub(camera.position).normalize();
     const distance = -camera.position.z / mouseDir.z + 12;
@@ -130,15 +137,14 @@
 
     // Move the styx
     cubes.forEach(cube => {
-
       const { rotation, position, userData } = cube;
       const { dir, vel, rotSpeed, maxVel, fric } = userData;
       const dt = Date.now() / 5000;
 
       // Rotation
-      rotation.x += rotSpeed * (Math.sin(dt));
-      rotation.y += rotSpeed * (Math.cos(dt));
-      rotation.z += rotSpeed * (Math.cos(dt));
+      rotation.x += rotSpeed * Math.sin(dt);
+      rotation.y += rotSpeed * Math.cos(dt);
+      rotation.z += rotSpeed * Math.cos(dt);
 
       // Velocity (is relative to distance from mouse)
       dir.subVectors(pos, position).normalize().multiplyScalar(0.01);
@@ -147,12 +153,9 @@
 
       // Friction
       vel.multiplyScalar(fric);
-
     });
 
     r.render(scene, camera);
     requestAnimationFrame(tick);
-
-  }(0));
-
-}(window.THREE));
+  })(0);
+})(window.THREE);
